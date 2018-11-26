98916dd3654f49733c68434cb202e9a1e4d15ecb
Isaac <saacman27@gmail.com>
2018-11-26 00:43:24 -0600

    Clase virtual para los estados de la FSM

--- state.hpp
+++ state.hpp
@@ -5,16 +5,20 @@
 
 class State {
 public:
+    // Se llama una vez para crear un estado inicialmente
     virtual void init() = 0;
+    // LLamado cuando se destruye un estados
     virtual void terminate() = 0;
+    // Llamado cuando cambia al estado
     virtual void activate() {}
+    // Llamado cuando cambia a otro estado
     virtual void deactivate() {}
 
     virtual void ProcessInput() {}
     virtual void Update( float dTime) {}
     virtual void LateUpdate(float dTime) {}
     virtual void Draw(Window& window) {}
-    
+
 };
 
 #endif
