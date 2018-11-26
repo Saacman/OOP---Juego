d2ab20ece9d8ef8c28b52a9ec7a1f3fc2761eed1
DarioCorona <45348430+DarioCorona@users.noreply.github.com>
2018-11-26 00:24:09 -0600

    archivos correspondientes al tutorial 7 y 8

---
+++ SceneGame.cpp
@@ -0,0 +1,39 @@
+#include "SceneGame.hpp"
+SceneGame::SceneGame(WorkingDirectory& workingDir) : workingDir(workingDir)
+{
+
+}
+
+void SceneGame::OnCreate()
+{
+    player = std::make_shared<Object>();
+    auto sprite = player->AddComponent<C_Sprite>();
+    sprite->Load(workingDir.Get() + "viking.png");
+    auto movement = player->AddComponent<C_KeyboardMovement>();
+    movement->SetInput(&input);
+}
+
+void SceneGame::OnDestroy()
+{
+
+}
+
+void SceneGame::ProcessInput()
+{
+    input.Update();
+}
+
+void SceneGame::Update(float deltaTime)
+{
+     player->Update(deltaTime);
+}
+
+void SceneGame::Draw(Window& window)
+{
+    player->Draw(window);
+}
+
+void SceneGame::LateUpdate(float deltaTime)
+{
+    player->LateUpdate(deltaTime);
+}
