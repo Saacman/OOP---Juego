0b06a66812d7377206945afae4e271fa5c0880e3
Isaac <saacman27@gmail.com>
2018-11-27 22:56:54 -0600

    Clase para las coordenadas de objetos

---
+++ transform.hpp
@@ -0,0 +1,31 @@
+#ifndef TRANSFORM_HPP
+#define TRANSFORM_HPP
+
+#include "component.hpp"
+
+class Transform : public Component
+{
+public:
+    Transform(Object* owner);
+
+    void SetPosition(float x, float y);
+    void SetPosition(const sf::Vector2f& pos);
+
+    void AddPosition(float x, float y);
+    void AddPosition(sf::Vector2f pos);
+
+    void SetX(float x);
+    void SetY(float y);
+
+    void AddX(float x);
+    void AddY(float y);
+
+
+
+    const sf::Vector2f& GetPosition() const;
+
+private:
+    sf::Vector2f position;
+};
+
+#endif
