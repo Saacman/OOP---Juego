bc16a1d14b21ac2a3b75f1879f3d51f85734c245
Isaac <saacman27@gmail.com>
2018-11-27 22:53:09 -0600

    Actualizado con los controles del jugador

--- gamestate.cpp
+++ gamestate.cpp
@@ -8,22 +8,23 @@ void GameState::init() {
     // Añadir componentes al jugador (objeto)
     auto sprite = player->AddComponent<Sprite>();
     sprite->Load(path.Get() + "viking.png");
+
+    auto movement = player->AddComponent<PlayerControl>();
+    movement->SetInput(&input);
+
 }
 void GameState::terminate() { }
 
 void GameState::ProcessInput() {
     input.Update();
 }
+
 void GameState::Update(float dTime) {
-    // const int speed = 150;
-    // float frameMove = speed * dTime;
-    // int xMove = 0;
-    // int yMove = 0;
-    // if (input.isKPressed(Input::Key::Up))  yMove = -frameMove;
-    // if (input.isKPressed(Input::Key::Down)) yMove = frameMove;
-    // if (input.isKPressed(Input::Key::Right)) xMove = frameMove;
-    // if (input.isKPressed(Input::Key::Left)) xMove = -frameMove;
-    // vikingSprite.move(xMove, yMove);
+    player->Update(dTime);
+}
+
+void GameState::LateUpdate(float dTime) {
+    player->LateUpdate(dTime);
 }
 void GameState::Draw(Window& window) {
     player->Draw(window);
--- gamestate.hpp
+++ gamestate.hpp
@@ -1,5 +1,5 @@
-#ifndef STATEAPP_HPP
-#define STATEAPP_HPP
+#ifndef STATEGAME_HPP
+#define STATEGAME_HPP
 
 #include "window.hpp"
 #include "r_path.hpp"
@@ -7,6 +7,7 @@
 #include "state.hpp"
 #include "object.hpp"
 #include "sprite.hpp"
+#include "playercontrol.hpp"
 
 class GameState : public State {
 public:
@@ -18,6 +19,7 @@ public:
     void ProcessInput() override;
     void Update(float dTime) override;
     void Draw(Window& window) override;
+    void LateUpdate(float dTime) override;
 
 private:
     std::shared_ptr<Object> player;
