b2148b312b2b2cb9951ecb9a812c79e733d5459c
Isaac <saacman27@gmail.com>
2018-11-27 20:21:40 -0600

    Añadido linea para compilar (hasta ahora) y object.cpp

--- main.cpp
+++ main.cpp
@@ -12,3 +12,4 @@ int main() {
     }
     return 0;
 }
+// g++ -std=c++11 -o test main.cpp app.cpp window.cpp r_path.cpp input.cpp bitmask.cpp gamestate.cpp fsm.cpp splashscreen.cpp -lsfml-graphics -lsfml-window -lsfml-system
